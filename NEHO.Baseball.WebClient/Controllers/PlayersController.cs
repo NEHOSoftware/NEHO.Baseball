11d93ef3a461d69f90e389d859f44ac5802be2e1
Jim Correll <jim.correll@nehosoftware.com>
2016-08-22 20:53:07 -0400

    Added ability to add batter and pitcher data to player view

--- NEHO.Baseball.WebClient/Controllers/PlayersController.cs
+++ NEHO.Baseball.WebClient/Controllers/PlayersController.cs
@@ -17,33 +17,29 @@ namespace NEHO.Baseball.WebClient.Controllers
         public async Task<ActionResult> Index()
         {
             var httpClient = BaseballHttpClient.GetClient();
-
-            var battersResponse = await httpClient.GetAsync("api/batters");
-
-            if (battersResponse.IsSuccessStatusCode)
-            {
-                var battersContent = await battersResponse.Content.ReadAsStringAsync();
-                var batters = JsonConvert.DeserializeObject<IEnumerable<Batter>>(battersContent);
-            }
-            else
-            {
-                return Content("An error ocurred.");
-            }
-
             var playersModel = new PlayersViewModel();
 
-            var httpResponseMessage = await httpClient.GetAsync("api/players");
+            var playersResponseMessage = await httpClient.GetAsync("api/players");
+            var battersResponseMessage = await httpClient.GetAsync("api/batters");
+            var pitchersResponseMessage = await httpClient.GetAsync("api/pitchers");
 
-            if (httpResponseMessage.IsSuccessStatusCode)
+            if (playersResponseMessage.IsSuccessStatusCode && battersResponseMessage.IsSuccessStatusCode && pitchersResponseMessage.IsSuccessStatusCode)
             {
-                var content = await httpResponseMessage.Content.ReadAsStringAsync();
-                var players = JsonConvert.DeserializeObject<IEnumerable<Player>>(content);
+                var playersContent = await playersResponseMessage.Content.ReadAsStringAsync();
+                var battersContent = await battersResponseMessage.Content.ReadAsStringAsync();
+                var pitchersContent = await pitchersResponseMessage.Content.ReadAsStringAsync();
 
-                playersModel.Players = players; 
+                var players = JsonConvert.DeserializeObject<IEnumerable<Player>>(playersContent);
+                var batters = JsonConvert.DeserializeObject<IEnumerable<Batter>>(battersContent);
+                var pitchers = JsonConvert.DeserializeObject<IEnumerable<Pitcher>>(pitchersContent);
+
+                playersModel.Players = players;
+                playersModel.Batters = batters;
+                playersModel.Pitchers = pitchers;
             }
             else
             {
-                return Content("An error ocurred.");
+                return Content("An error ocurred while retrieving data.");
             }
 
             return View(playersModel);
